3f6c372318d7de1d5669c9cfd5d6d39ad0256c70
恶妇 <scribal.tuneups.0c@icloud.com>
2024-11-16 12:53:04 +0000

    Update install-and-run.ps1

--- install-and-run.ps1
+++ install-and-run.ps1
@@ -1,12 +1,3 @@
-# Let's make sure we can run this project
-Set-ExecutionPolicy Bypass -Scope Process -Force
-
-# Use a TLS 1.2 connection
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-
-# Install chocolatey
-iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-
 # Get refreshenv
 Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 
@@ -16,10 +7,6 @@ refreshenv
 # Auto confirm
 choco feature enable -n=allowGlobalConfirmation
 
-# Install CMake
-choco install cmake
-refreshenv
-
 # Install Visual Studio 2019 build tools
 choco install visualstudio2019buildtools --notsilent --package-parameters "--includeRecommended --includeOptional"
 refreshenv
@@ -27,10 +14,6 @@ refreshenv
 # Install MSVC C++ Workloads
 Write-Output "Install MSVC 2019 C++ workloads"
 choco install visualstudio2019-workload-vctools --quiet --package-parameters "--add Microsoft.VisualStudio.Component.VC.ATLMFC --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64"
-refreshenv
-
-Write-Output "Install MSVC 2019 C++ Native Desktop workload"
-choco install visualstudio2019-workload-nativedesktop
 refreshenv
 
 # Initialise the VC variables
